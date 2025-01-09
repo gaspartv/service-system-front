233e206508e338c32ef2026133a6767f00d4105b
Diego Gaspar <cadastro@diegogaspar.dev.br>
2025-01-08 23:40:45 -0300

    fix: adicionado icone para encolher sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -8,13 +8,16 @@ import KeyIcon from "@mui/icons-material/Key";
 import Grid3x3Icon from "@mui/icons-material/Grid3x3";
 import SettingsIcon from "@mui/icons-material/Settings";
 import SignOutButton from "@/app/dashboard/components/sign-out-button";
+import ViewHeadlineIcon from '@mui/icons-material/ViewHeadline';
+import KeyboardDoubleArrowLeftIcon from '@mui/icons-material/KeyboardDoubleArrowLeft';
 import React from "react";
 
 export default function Sidebar() {
   return (
     <div
-      className="flex flex-col justify-between p-2 gap-0.5 w-48 absolute top-0 h-screen text-xs bg-zinc-800 border-r border-zinc-700">
-      <div>
+      className="flex flex-col justify-between p-2 gap-0.5 w-48 min-w-48 h-screen text-xs bg-zinc-800 border-r border-zinc-700">
+      <div className="flex flex-col gap-1">
+        <ViewHeadlineIcon className="cursor-pointer" />
         <LinkMenu path={"/dashboard"} name={"Início"} icon={<HomeIcon />} />
         <LinkMenu path={"/customer-service"} name={"Atendimentos"} icon={<WhatsAppIcon />} />
         <LinkMenu path={"/customer-service-history"} name={"Históricos"} icon={<ReceiptIcon />} />
